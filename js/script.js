daafba647aae07da31abf818e3b1bec2950efe3b
audyningrum27 <66819730+audyningrum27@users.noreply.github.com>
2020-08-07 17:41:45 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('button');
 const body = document.querySelector('body');
 const output = document.getElementById('output');
-
+const satu = document.getElementById('satu');
 
 const randomNumber = (colorLib = 16777216) => {
     return Math.floor(Math.random() * Math.floor(colorLib));
@@ -9,25 +9,37 @@ const randomNumber = (colorLib = 16777216) => {
 
 const salam = [
     "Halo",
-    "Apa Kabar?",
+    "Apa kabar?",
     "Hello",
-    "How are You?",
+    "How are you?",
     "السلام عليكم",
     "كيف حالك؟",
     "안녕하세요",
     "어떻게 지내?"
-]
+];
+
+const getNewColor = () => {
+    let symbols, color;
+    symbols = "0123456789ABCDEF"
+    color = "#"
+    for(let i = 0; i < 6; i++) {
+        color += symbols[Math.floor(Math.random() * 16)];
+    }
+    body.style.backgroundColor = color;
+    satu.innerHTML = color;
+}
 
 const randomColor = () => {
-    body.style.backgroundColor = `#${randomNumber().toString(16)}`;
+//    body.style.backgroundColor = `#${randomNumber().toString(16)}`;
+//    button.style.boxShadow = `#${Math.abs(8388608 - randomNumber()).toString(16)}`;
     button.style.backgroundColor = `#${(16777216 - randomNumber()).toString(16)}`;
     button.style.border = `0.5vh solid #${Math.abs(8388608 - randomNumber()).toString(16)}`;
-//    button.style.boxShadow = `#${Math.abs(8388608 - randomNumber()).toString(16)}`;
     button.style.color = `#${randomNumber().toString(16)}`;
 }
 
 button.addEventListener('click', () => {
     randomColor();
+    getNewColor();
     const randomSalam = salam[Math.floor(Math.random() * salam.length)];
     output.innerHTML = randomSalam;
 });
